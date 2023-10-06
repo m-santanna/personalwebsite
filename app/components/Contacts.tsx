0c0e809f66634300cf9e4cd5ac2495cb3c24b356
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-10-06 23:30:55 +0100

    about and contact features added

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -1,5 +1,20 @@
+import ContactLinks from "../icons/ContactLinks";
+import Email from "../icons/Email";
+
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      <h1>Contact me!</h1>
+      <h3>Email</h3>
+      <a href="mailto:matheus.santanna03@gmail.com" target="_blank">
+        matheus.santanna03@gmail.com
+      </a>
+      <h3>Linkedin</h3>
+      <a href="https://www.linkedin.com/in/m-santanna/" target="_blank">
+        m-santanna
+      </a>
+    </div>
+  );
 }
 
 export default Contacts;
