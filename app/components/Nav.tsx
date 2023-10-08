bb58a1f16493e0030344e8aa3544b559d2ec2e29
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-10-08 16:33:55 +0100

    small update on the nav bar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import Menu from "../icons/Menu";
 import DarkModeToggle from "./DarkModeToggle";
 
 export default function Nav() {
   return (
     <nav className="flex justify-between items-center p-4 bg-super-strong border border-x-0 border-t-0 border-stone-800">
-      <div className="text-super-weak first-letter:text-stone-200 text-2xl">
+      <Link
+        href={"/"}
+        className="text-super-weak first-letter:text-stone-200 text-2xl"
+      >
         Matheus.
-      </div>
+      </Link>
       <div>
         <DarkModeToggle className="pr-4 text-super-weak" />
         <Menu className="pr-2 text-super-weak" />
