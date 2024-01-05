ccaf3b6a843585d933e23bdc7d4af86b2504360b
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2024-01-05 02:47:36 +0000

    scrolldown icon updated v3

--- app/sections/HomePage.tsx
+++ app/sections/HomePage.tsx
@@ -2,7 +2,7 @@ import ScrollDown from "../components/ScrollDown";
 
 function HomePage() {
   return (
-    <div className="w-screen h-[calc(55vh)] sm:h-[calc(100vh-80px)]">
+    <div className="w-screen h-[calc(90vh-80px)] sm:h-[calc(100vh-80px)]">
       <div className="bg-strong mt-[5vh] sm:mt-[15vh] mx-[10vw] flex items-center justify-center text-center ring-[0.03rem] ring-stone-700 shadow-lg shadow-stone-700">
         <div className="p-16">
           <p className="text-4xl mt-[1rem] mb-4 text-super-weak font-semibold">
