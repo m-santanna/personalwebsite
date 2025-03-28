159f57304992d659a7aeddd5020be2a5fcff7732
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-03-28 19:42:51 +0000

    improve resume and certificate style on mobile

--- app/cs50x/page.tsx
+++ app/cs50x/page.tsx
@@ -4,8 +4,12 @@ import cs50x from "../images/CS50x.png";
 
 function page() {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <Image className="w-2/3" src={cs50x} alt="cs50x's certificate" />
+    <div className="flex justify-center items-center h-screen w-screen p-8">
+      <Image
+        className="w-full lg:w-2/3"
+        src={cs50x}
+        alt="cs50x's certificate"
+      />
     </div>
   );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 const ResumePage = () => {
   return (
-    <div className="flex justify-center p-8 w-screen">
-      <Image className="w-1/2" src={resume} alt="resume" />
+    <div className="flex justify-center items-center p-8 w-screen">
+      <Image className="w-full lg:w-1/2" src={resume} alt="resume" />
     </div>
   );
 };
