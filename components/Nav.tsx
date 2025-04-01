f80e045c495852ea8a1cd36e2ca27b91f815018e
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-04-01 22:54:47 +0100

    navbar update

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,22 +7,16 @@ export default function Nav() {
       <nav className="bg-navbar flex w-full justify-between items-center p-4 h-20 border border-x-0 border-t-0 border-stone-800 dark:border-gray-400">
         <Link
           href={"#HomePage"}
-          className="text-super-weak first-letter:text-gray-800 dark:first-letter:text-stone-200 text-2xl"
+          className="text-super-weak first-letter:text-gray-800 dark:first-letter:text-stone-200 text-2xl font-semibold"
         >
           MSS.
         </Link>
-        <div className="flex gap-4">
-          <Link href={"#About"} className="text-super-weak">
-            About
-          </Link>
-          <Link href={"#Projects"} className="text-super-weak">
-            Projects
-          </Link>
-          <Link href={"#Contacts"} className="text-super-weak">
-            Contacts
-          </Link>
-          <DarkModeToggle className="text-super-weak" />
+        <div className="flex items-center gap-4 text-super-weak font-extralight text-sm sm:text-lg">
+          <Link href={"#About"}>About</Link>
+          <Link href={"#Projects"}>Projects</Link>
+          <Link href={"#Contacts"}>Contacts</Link>
         </div>
+        <DarkModeToggle />
       </nav>
     </header>
   );
