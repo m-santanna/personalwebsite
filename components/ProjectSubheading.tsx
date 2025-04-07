8ecfb25c4249d30c536f2f775cf02887eb3f0757
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2025-04-07 18:38:35 +0100

    update on hover effect of project subheadings

--- components/ProjectSubheading.tsx
+++ components/ProjectSubheading.tsx
@@ -14,12 +14,12 @@ function ProjectSubheading(props: ProjectSubheadingProps) {
       <div className="mb-2">
         <div className="flex items-center">
           <Link
-            className="flex items-center gap-2 subheading underline-animation"
+            className="group flex items-center gap-2 subheading bg-gradient-to-bl from-amber-500 to-amber-800 hover:text-transparent bg-clip-text"
             href={props.href}
             target="_blank"
           >
             {props.title}
-            <Link2 className="size-6 md:size-8" />
+            <Link2 className="size-6 md:size-8 text-current group-hover:text-amber-600" />
           </Link>
         </div>
         <p className="font-thin">{props.date}</p>
